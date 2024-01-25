5b3f6bd97109677325260335419dc3fb5a38b5ba
ThanhPhong212 <bienthanhphong212@gmail.com>
2024-01-25 16:33:42 +0700

    add code

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
-const { storeUser, getUser, getRoomUser, userLeave } = require("./services/users");
-const { formatMess } = require("./services/message");
 const cors = require("cors");
 
 const app = express();
@@ -29,38 +27,20 @@ const io = socketio(server, {
 });
 
 io.on("connection", (socket) => {
-  socket.on("userJoinRoom", ({ username, room }) => {
-    socket.join(room);
-    io.to(room).emit("serverMessage", formatMess(botName, `Chào mừng <b>${username}</b> vào phòng <b>${room}</b>`));
-    storeUser(socket.id, username, room);
+  const courseId = socket.handshake.query.courseId;
+  socket.join(courseId);
 
-    io.to(room).emit("roomUsers", {
-      room: room,
-      users: getRoomUser(room),
-    });
+  socket.on("sendMessServer", (data) => {
+    io.to(courseId).emit("sendMessClient", data);
   });
 
-  socket.on("chatMessage", (message) => {
-    const user = getUser(socket.id);
-    const mesObj = formatMess(user.username, message);
-    io.to(user.room).emit("serverMessage", mesObj);
+  socket.on("sendReplyServer", (data) => {
+    io.to(courseId).emit("sendReplyClient", data);
   });
 
-  socket.on("disconnect", () => {
-    if (socket.id) {
-      const user = userLeave(socket.id);
-      if (user && user.room) {
-        io.to(user.room).emit("serverMessage", formatMess(botName, `<b>${user.username}</b> đã rời phòng`));
-
-        // Gửi thông tin phòng và danh sách tất cả users trong phòng
-        io.to(user.room).emit("roomUsers", {
-          room: user.room,
-          users: getRoomUser(user.room),
-        });
-      }
-    }
-  });
 });
+
+app.get("/", (req, resp) => resp.send("socket-v1"));
 
 server.listen(8000, () => {
   console.log(`Server is running`);
